096450e2518936b7e5cdbd80337720723ef1695f
giorguna2000 <natia.abzhandadze@mail.ru>
2024-11-13 21:41:31 +0400

    commit

---
+++ day 06/homework/hw2.py
@@ -0,0 +1,8 @@
+# 2) მომხმარებელს შეეკითხეთ სახელი, გვარი, ასაკი, საცხოვრებელი ქალაქი,ჰობი შემდეგ კი დაბეჭდეთ გრძელი წინადადება მაგ. შენი სახელია "სახელი და გვარი" შენი ასაკია "ასაკი" და ასე შემდეგ.
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+location = input("enter your location: ")
+city = input("enter your city: ")
+hobby = input("enter your hobby: ")
+print(name,lastname,age,location,city,hobby  )
