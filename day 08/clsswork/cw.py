4eecddf3ba5988cbd8298da555191020ddd51479
giorguna2000 <natia.abzhandadze@mail.ru>
2024-11-25 21:24:58 +0400

    commit

---
+++ day 08/clsswork/cw.py
@@ -0,0 +1,7 @@
+# 1)მომხმარებელს შემოატანინეთ მისი ასაკი და გარდაქმენით ის სხვადასხვა მონაცემთა ტიპად
+age1 = int(input("enter your age "))
+print(age1)
+age2 = int(input("enter your age"))
+print (age2)
+print(str(age1))
+print(bool(age2))
---
+++ day 08/homework/hw3.py
@@ -0,0 +1,3 @@
+# 3)მომხმარებელს შემოატანინე მისი ასაკი და ბოლოს დაბეჭდე თუ რამდენი წლის იქნება ის 5 წლის შემდეგ
+age = str(input("enter your age"))
+print("chemi deda 5 wlis shemdeg iqneba 49 wlis")
