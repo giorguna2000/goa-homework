8963c057231855114c4ffb757f6ac4d36f2f498d
giorguna2000 <natia.abzhandadze@mail.ru>
2024-12-21 22:10:19 +0400

    commit

---
+++ day 12/classwork/cw.py
@@ -0,0 +1,23 @@
+# შექმენით 10 ცვლადი და მიანიჭთ მნიშვნელობები მაგალითებით (მაგალითად 7 > 9) და შემდეგ დაპრინტეთ True და False სხვა და სხვა კომბინაციით (კომენტარებით მიუწერეთ გვერდით რას გამოიტანს ეკრანზე)
+number1 = (2>3)#False
+number2 = (7==7)#True 
+number3 = (3<8)#True 
+number4 = (5==5)#True 
+number5 = (5>5)#False
+number6 = (2>4)#False
+number7 = (2>10)#False
+number8= (6>4)#True
+number9 = (7>4)#True
+number10 = (100==100)#True
+print(number1)
+print(number2)
+print(number3)
+print(number3)
+print(number4)
+print(number5)
+print(number6)
+print(number7)
+print(number8)
+print(number9)
+print(number10)
+
