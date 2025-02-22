866f51413764a8bab4bfedc60439a944af37c0b6
giorguna2000 <giorgismile454@gmail.com>
2025-02-22 18:17:27 +0400

    commit

---
+++ day 20/homework/hw3.py
@@ -0,0 +1,7 @@
+# 3)ცვლადში შეინახეთ თქვენი სახელი, მომხმარებელს შემოატანინეთ მისი სახელი და თუ თქვენი სახელები ემთხვევა დაუპრინტეთ "Hello" სხვა შემთხვევაში "Bye"
+my_name = "giorgi"
+user_name = input("Enter your name: ")
+if my_name == user_name:
+    print("Hello")
+else:
+    print("Bye")
