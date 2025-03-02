440dac7231c77bbee38da5852ed0d78665199728
giorguna2000 <giorgismile454@gmail.com>
2025-03-02 22:11:54 +0400

    commit

---
+++ day 22/classwork/cw3.py
@@ -0,0 +1,7 @@
+# 3) ცვლადში შეინახეთ თქვენი საყვარელი ფილმის სახელი, შემდეგ კი მომხმარებელს შემოატანინეთ მისი საყვარელი ფილმი. იმ შემთხვევაში თუ თქვენი და მომხმარებლის საყვარელი ფილმები ემთხვევა - გამოიტანეთ "our movie taste is similar", სხვა შემთხვევაში კი გამოიტანეთ "we like different movies"
+my_favorite_movie = "Inception"
+user_favorite_movie = input("Please enter your favorite movie: ")
+if my_favorite_movie == user_favorite_movie:
+    print("Our movie taste is similar.")
+else:
+    print("We like different movies.")
